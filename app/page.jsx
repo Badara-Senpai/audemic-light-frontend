54453e2f909f53c972637a1d0bc2dcea7239a701
Badara <badou017@gmail.com>
2024-01-14 20:20:17 +0000

    Uploading File Feature

---
+++ app/page.jsx
@@ -0,0 +1,63 @@
+"use client"
+
+import Image from 'next/image'
+import {useState} from "react";
+import RecentFile from "@/app/component/recent_files";
+import RecentFiles from "@/app/component/recent_files";
+
+export default function Home() {
+    const [file, setFile] = useState(null);
+
+    const handleFileChange = (event) => {
+        setFile(event.target.files[0]);
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        const formData = new FormData();
+        formData.append('file', file);
+
+        const response = await fetch('http://localhost:8000/upload/', {
+            method: 'POST',
+            body: formData,
+        });
+
+    };
+
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+          <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+              <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+                  Get started by editing&nbsp;
+                  <code className="font-mono font-bold">app/page.js</code>
+              </p>
+              <div
+                  className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+                  <a
+                      className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+                      href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
+                      target="_blank"
+                      rel="noopener noreferrer"
+                  >
+                      By{' '}
+                      <Image
+                          src="/vercel.svg"
+                          alt="Vercel Logo"
+                          className="dark:invert"
+                          width={100}
+                          height={24}
+                          priority
+                      />
+                  </a>
+              </div>
+          </div>
+
+          <form onSubmit={handleSubmit}>
+              <input type="file" onChange={handleFileChange}/>
+              <button type="submit">Upload</button>
+          </form>
+
+          <RecentFiles/>
+      </main>
+  )
+}
